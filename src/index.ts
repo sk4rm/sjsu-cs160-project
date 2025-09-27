211b29737dff1de96fe71598dd20a2f23aa11279
Zi Hong <74897601+sk4rm@users.noreply.github.com>
2025-09-27 12:43:39 -0700

    Implement basic HTTP server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,61 @@
-import { Elysia } from "elysia";
+import { Elysia, file, t } from "elysia";
+import { openapi } from "@elysiajs/openapi";
+import { staticPlugin } from "@elysiajs/static";
+import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+if (Bun.env.URI_MONGO === undefined) {
+    throw new Error("Environment variable URI_MONGO not specified.");
+}
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+const client = new MongoClient(Bun.env.URI_MONGO, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true
+    }
+});
+const db = client.db("eco-leveling");
+
+const app = new Elysia()
+    .use(staticPlugin())
+
+    .use(openapi({
+        exclude: {
+            paths: ["/public/*"]
+        }
+    }))
+
+    .get("/favicon.ico", () => file("favicon.ico"))
+
+    .group("/api", (api) =>
+        api
+            .get("/users/:id", ({ params }) => {
+                return db
+                    .collection("users")
+                    .findOne({
+                        _id: new ObjectId(params.id)
+                    });
+            }, {
+                params: t.Object({
+                    id: t.String()
+                })
+            })
+
+            // Test with
+            // `irm -Uri http://127.0.0.1:3000/api/users -Method POST -ContentType "application/json" -Body (ConvertTo-Json -InputObject @{ name = "Jane Doe"  })`
+            .post("/users", ({ body }) => {
+                return db
+                    .collection("users")
+                    .insertOne({ name: body.name });
+            }, {
+                body: t.Object({
+                    name: t.String()
+                })
+            })
+    )
+
+    .listen(3000);
+
+console.info(
+    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
