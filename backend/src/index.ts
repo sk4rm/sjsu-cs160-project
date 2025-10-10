da75ae59f400eb142cf67662cc7560ad7cd78c53
Zi Hong <74897601+sk4rm@users.noreply.github.com>
2025-10-10 13:33:38 -0700

    Add more fields to user

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
-import {Elysia, file, t} from "elysia";
-import {openapi} from "@elysiajs/openapi";
-import {staticPlugin} from "@elysiajs/static";
-import {MongoClient, ObjectId, ServerApiVersion} from "mongodb";
+import { Elysia, file, t } from "elysia";
+import { openapi } from "@elysiajs/openapi";
+import { staticPlugin } from "@elysiajs/static";
+import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
 
 if (Bun.env.URI_MONGO === undefined) {
     throw new Error("Environment variable URI_MONGO not specified.");
@@ -26,73 +26,86 @@ const app = new Elysia()
     .get("/favicon.ico", () => file("favicon.ico"))
 
     .group("/api", (api) => api
-        .post("/users", ({body}) => {
-            return db
-                .collection("users")
-                .insertOne({name: body.name});
-        }, {
-            body: t.Object({
-                name: t.String()
-            })
-        })
 
-        .get("/users/:id", ({params}) => {
-            return db
-                .collection("users")
-                .findOne({
-                    _id: new ObjectId(params.id)
-                });
-        }, {
-            params: t.Object({
-                id: t.String()
-            })
-        })
+        .group("/users", (users_api) => users_api
 
-        .patch("/users/:id", ({params, body}) => {
-            const query = {_id: new ObjectId(params.id)};
-            const update = {
-                $set: {
-                    name: body.name,
-                }
-            };
-            const options = {};
-
-            return db
-                .collection("users")
-                .updateOne(query, update, options);
-        }, {
-            params: t.Object({id: t.String()}), body: t.Object({
-                name: t.String()
+            .post("/", ({ body }) => {
+                return db
+                    .collection("users")
+                    .insertOne({
+                        name: body.name,
+                        bio: body.bio,
+                        password: body.password,
+                        points: body.points,
+                        profile_pic_url: body.profile_pic_url,
+                        is_moderator: body.is_moderator
+                    });
+            }, {
+                body: t.Object({
+                    name: t.String(),
+                    bio: t.Optional(t.String()),
+                    password: t.String(),
+                    points: t.Optional(t.Integer()),
+                    profile_pic_url: t.Optional(t.String()),
+                    is_moderator: t.Optional(t.Boolean())
+                })
             })
-        })
 
-        .delete("/users/:id", ({params}) => {
-            return db
-                .collection("users")
-                .deleteOne({
-                    _id: new ObjectId(params.id)
-                });
-        }, {
-            params: t.Object({
-                id: t.String()
+            .get("/:id", ({ params }) => {
+                return db
+                    .collection("users")
+                    .findOne({
+                        _id: new ObjectId(params.id)
+                    });
+            }, {
+                params: t.Object({
+                    id: t.String()
+                })
             })
-        }))
+
+            .patch("/:id", ({ params, body }) => {
+                const query = { _id: new ObjectId(params.id) };
+                const update = {
+                    $set: {
+                        name: body.name
+                    }
+                };
+                const options = {};
+
+                return db
+                    .collection("users")
+                    .updateOne(query, update, options);
+            }, {
+                params: t.Object({
+                    id: t.String()
+                }),
+                body: t.Object({
+                    name: t.Optional(t.String()),
+                    bio: t.Optional(t.String()),
+                    password: t.Optional(t.String()),
+                    points: t.Optional(t.Integer()),
+                    profile_pic_url: t.Optional(t.String()),
+                    is_moderator: t.Optional(t.Boolean())
+                })
+            })
+
+            .delete("/:id", ({ params }) => {
+                return db
+                    .collection("users")
+                    .deleteOne({
+                        _id: new ObjectId(params.id)
+                    });
+            }, {
+                params: t.Object({
+                    id: t.String()
+                })
+            })
+
+        )
+
+    )
 
     .listen(3000);
 
 console.info(`🍂 Eco-Leveling backend is running at http://${app.server?.hostname}:${app.server?.port}`);
 console.info(`⚙️ Please refer to API documentation at http://${app.server?.hostname}:${app.server?.port}/openapi`);
-
-
-//tester
-// import { Elysia } from 'elysia'
-
-// const app = new Elysia()
-
-// app.get('/api/user', () => ({
-//   name: 'Eco',
-//   role: 'developer'
-// }))
-
-// app.listen(3000)
-// console.log('🦊 Elysia running on http://localhost:3000')
